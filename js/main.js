29d739c945e6c8d4c12ce38892ef2defa67ec4d6
Ivan Ramos <mivaan.ctes@gmail.com>
2023-09-12 19:56:44 -0300

    Modificacion del archivo main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
-(function ($) {
-
+$(document).ready(function () {
     // Scroll to Bottom
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('.scroll-to-bottom').fadeOut('slow');
+            $('.scroll-to-bottom').fadeOut('fast');
         } else {
-            $('.scroll-to-bottom').fadeIn('slow');
+            $('.scroll-to-bottom').fadeIn('fast');
         }
     });
 
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
+            $('.back-to-top').fadeIn('fast');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $('.back-to-top').fadeOut('fast');
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 2500, 'swing');
         return false;
     });
-
-})(jQuery)
+})  
